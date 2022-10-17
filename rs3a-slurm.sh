21c3240306b5dde2345b4627cdc54676712362ff
AWS ParallelCluster user <ec2-user@ip-10-0-11-216.ap-southeast-1.compute.internal>
2022-10-17 09:06:22 +0000

    new update

--- rs3a-slurm.sh
+++ rs3a-slurm.sh
@@ -22,4 +22,4 @@ print(time.localtime()); \
 DPT.objects.processDirs(dirs=None, objtype=pyh.RPLSplit, channel=[*range(65,97)], SkipHPC=False, HPCScriptsDir = '/data/src/PyHipp/', SkipLFP=False, SkipHighPass=False, SkipSort=False); \
 print(time.localtime()); \
 print(time.time()-t0);"
-aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:018084650241:awsnotify --message "RPLS3JobDone"
+aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:259117019060:awsnotify --message "RPLS3JobDone"
--- rse-slurm.sh
+++ rse-slurm.sh
@@ -22,4 +22,4 @@ os.chdir('sessioneye'); \
 pyh.RPLSplit(SkipLFP=False, SkipHighPass=False); \
 print(time.localtime()); \
 print(time.time()-t0);"
-aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:018084650241:awsnotify --message "RPLSEJobDone"
+aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:259117019060:awsnotify --message "RPLSEJobDone"
